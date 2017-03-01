ec67e672d78b7b27d3fa1efeaa3ac481c994fad3
Rulan Gong <rgong3@gatech.edu>
2017-03-01 01:09:11 -0600

    Finished deck/player/prize/tournamentResult

--- GroupProject/TourneyManager/app/src/main/java/edu/gatech/seclass/tourneymanager/MainActivity.java
+++ GroupProject/TourneyManager/app/src/main/java/edu/gatech/seclass/tourneymanager/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         players.add("Yu Wang");
         players.add("Don Lee");
 
+
         ArrayAdapter<String> arrayAdapter =
                 new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, players);
 
