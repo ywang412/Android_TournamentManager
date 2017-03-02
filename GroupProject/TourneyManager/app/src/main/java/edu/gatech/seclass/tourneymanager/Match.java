ecbbf8ee1bd7fd7c4c942b246ce3ea688faeaeee
Wang, Yu <yuwang@gatech.edu>
2017-03-02 16:45:33 -0600

    Update Match.java

--- GroupProject/TourneyManager/app/src/main/java/edu/gatech/seclass/tourneymanager/Match.java
+++ GroupProject/TourneyManager/app/src/main/java/edu/gatech/seclass/tourneymanager/Match.java
@@ -9,30 +9,33 @@ import edu.gatech.seclass.tourneymanager.Status;
 public class Match {
 
         private int match_id;
-        private int next_match_id;
-        private int tournament_id;
-        private Status Mstatus;
+        private Tournament tournament;
+        private Status m_status;
         private int match_round;
         private Player winner;
+        private int next_match_id;
         private Player player_1;
         private Player player_2;
 
         public Match() {
         }
 
-        public Match(int match_id,int tournament_id,int match_round,Player player1, Player player2) {
+        public Match(int match_id,Tournament tournament_id,int match_round,Player player1, Player player2) {
             this.match_id = match_id;
-            this.tournament_id = tournament_id;
+            this.tournament = tournament_id;
             this.match_round = match_round;
             this.player_1 = player1;
             this.player_2 = player2;
+            m_status = Status.Setup;
+           // System.out.println(match_round+"d"+ match_id+ player1.getName()+ player2.getName()+m_status);
         }
 
-        public Match(int match_id, int tournament_id, int match_round) {
-        this.match_id = match_id;
-        this.match_round=match_round;
-        this.tournament_id = tournament_id;
-
+        public Match(int match_id, Tournament tournament_id, int match_round) {
+            this.match_id = match_id;
+            this.match_round=match_round;
+            this.tournament = tournament_id;
+            m_status = Status.Setup;
+           // System.out.println(match_round+"d"+ match_id+m_status);
         }
 
     public void startmatch() {
@@ -46,61 +49,7 @@ public class Match {
 //      MatchDA.addmatch(this);
     }
 
-    public int getMatch_id() {
-        return match_id;
-    }
 
-    public void setMatch_id(int match_id) {
-        this.match_id = match_id;
-    }
-
-    public int getTournament_id() {
-        return tournament_id;
-    }
-
-    public void setTournament_id(int tournament_id) {
-        this.tournament_id = tournament_id;
-    }
-
-    public Status getMstatus() {
-        return Mstatus;
-    }
-
-    public void setMstatus(Status mstatus) {
-        this.Mstatus = mstatus;
-    }
-
-    public int getMatch_round() {
-        return match_round;
-    }
-
-    public void setMatch_round(int match_round) {
-        this.match_round = match_round;
-    }
-
-    public Player getWinner() {
-        return winner;
-    }
-
-    public void setWinner(Player winner) {
-        this.winner = winner;
-    }
-
-    public Player getPlayer_1() {
-        return player_1;
-    }
-
-    public void setPlayer_1(Player player_1) {
-        this.player_1 = player_1;
-    }
-
-    public Player getPlayer_2() {
-        return player_2;
-    }
-
-    public void setPlayer_2(Player player_2) {
-        this.player_2 = player_2;
-    }
 }
 
 
