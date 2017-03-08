13d97b50c8530bbe03f8bcef726fe22949ade7f6
Don Lee <dlee317@gatech.edu>
2017-03-08 00:05:23 -0500

    add form to create tournament activity

--- GroupProject/TourneyManager/app/src/main/java/edu/gatech/seclass/tourneymanager/tournament/TournamentDetailsActivity.java
+++ GroupProject/TourneyManager/app/src/main/java/edu/gatech/seclass/tourneymanager/tournament/TournamentDetailsActivity.java
@@ -2,6 +2,7 @@ package edu.gatech.seclass.tourneymanager.tournament;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import edu.gatech.seclass.tourneymanager.R;
 
@@ -11,5 +12,7 @@ public class TournamentDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tournament_details);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
